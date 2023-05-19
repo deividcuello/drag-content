e4531a2783136f52d05b674a9d46b9a560c3c5ab
unknown <deividcuellocuevas@gmail.com>
2023-05-19 15:26:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Drag Content
+
+![image](https://github.com/deividcuello/drag-content/assets/112868702/5740126e-4692-4cf3-9eb9-a37f7beea769)
---
+++ script.js
@@ -0,0 +1,35 @@
+const headerSpan = document.querySelector('header span'),
+content = document.querySelector('.content'),
+showBtn = document.querySelector('.show-content button'),
+closeBtn = document.querySelector('svg'),
+dragContainer = document.querySelector('.drag-container')
+
+let drag = false
+
+showBtn.addEventListener('click', () => {
+    dragContainer.classList.toggle('show')
+})
+
+closeBtn.addEventListener('click', () => {
+    dragContainer.classList.remove('show')
+})
+
+headerSpan.addEventListener('mousedown', (e) => {
+    drag = true
+
+})
+
+window.document.addEventListener('mouseup', (e) => {
+    drag = false
+})
+
+document.querySelector('body').addEventListener('mousemove', (e) => {
+    if(!drag) return
+    console.log(e.pageY)
+    if(e.pageY > 600) return
+    if(e.pageY < 5)  content.style.borderRadius = '0px'
+    else content.style.borderRadius = '2rem 2rem 0 0'
+    
+    content.style.height = `${window.innerHeight - e.pageY}px`
+    
+})
